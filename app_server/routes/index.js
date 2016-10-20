e4f11c9311653af161bf2d0f72365ef9cfa0950f
Will Bean <will.bean@iknowdev.com>
2016-10-20 05:52:57 -0400

    Taking the controller code out of the route.

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var homepageController = function (req, res) {
+	res.render('index', {title: 'Express'});
+};
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', homepageController);
 
 module.exports = router;
