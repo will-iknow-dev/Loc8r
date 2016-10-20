38fd164116317a909a38357bd6747b70d3908b48
Will Bean <will.bean@iknowdev.com>
2016-10-20 06:02:37 -0400

    Setup homepage controller in app_server/controllers/main.js and updated the routes file to use external controllers.

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var homepageController = function (req, res) {
-	res.render('index', {title: 'Express'});
-};
+var ctrlMain = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', homepageController);
+router.get('/', ctrlMain.index);
 
 module.exports = router;
